56eba828f05dd5a3c0362c621fda8360da3c8c87
sameplacewei <sameplace@126.com>
2024-01-09 15:13:15 +0800

    change signer

--- types/signer.go
+++ types/signer.go
@@ -4,6 +4,7 @@ import (
 	"math/big"
 
 	evmTypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/params"
 )
 
 const (
@@ -11,7 +12,11 @@ const (
 	Signer_Cancun
 )
 
-func GetSigner(signerType uint8, chainId *big.Int) evmTypes.Signer {
+func DefaultSignerType() uint8 {
+	return Signer_London
+}
+
+func MakeSigner(signerType uint8, chainId *big.Int) evmTypes.Signer {
 	switch signerType {
 	case Signer_London:
 		return evmTypes.NewLondonSigner(chainId)
@@ -19,4 +24,20 @@ func GetSigner(signerType uint8, chainId *big.Int) evmTypes.Signer {
 		return evmTypes.NewCancunSigner(chainId)
 	}
 	return nil
+}
+
+func GetSignerType(height *big.Int, config *params.ChainConfig) uint8 {
+	var signerType uint8
+
+	switch {
+	case isCancun():
+		signerType = Signer_Cancun
+	default:
+		signerType = Signer_London
+	}
+	return signerType
+}
+
+func isCancun() bool {
+	return false
 }
